da5dbc7ff3adfce0b6a5e7778131c42a84c36127
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-04-15 23:41:04 -0700

    updates

--- slient_killer/code/GM.gd
+++ slient_killer/code/GM.gd
@@ -1,1 +1,13 @@
 extends Node3D
+
+var evo_points = 0;
+
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Tap"):
+		if evo_points != 100:
+			evo_points += 1;
+			$Gui/Evo_points/ProgressBar.value = evo_points
+			$Gui/Evo_points/Counter.text = str(evo_points)
