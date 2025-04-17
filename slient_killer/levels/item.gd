503755c6cebe0761cca8078e54e4e471d254b137
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-04-16 23:51:13 -0700

    updates

--- slient_killer/levels/item.gd
+++ slient_killer/levels/item.gd
@@ -1,1 +1,15 @@
 extends Control
+
+@export var cost = 0;
+
+func _on_button_button_down() -> void:
+	if Globals.evo_points >= cost:
+		Globals.evo_points -= cost;
+		$Cing.play();
+	else:
+		$error_msg.visible = true;
+		$Error.play();
+		$Error_timer.start();
+
+func _on_error_timer_timeout() -> void:
+	$error_msg.visible = false;
